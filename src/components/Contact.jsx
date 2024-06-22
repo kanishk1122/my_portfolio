1ef50394aa1583f53d517ff42154bd768a1e6856
kanishk soni <102342556+kanishk1122@users.noreply.github.com>
2024-06-22 21:16:29 +0530

    update

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,26 +8,46 @@ const Contact = () => {
     mobile: '',
     message: ''
   });
+  const [response, setresponse] = useState("")
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     try {
       const response = await axios.post('https://about-me-update-1.onrender.com/send-email', formData);
-      console.log('Email sent successfully:', response.data);
+      setresponse('Email sent successfully');
       // Optionally handle success response here
     } catch (error) {
-      console.error('Failed to send email:', error);
+      setresponse('Failed to send email');
       // Optionally handle error response here
     }
   };
+
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
+const checkvalue =()=>{
+    
+        setTimeout(()=>setresponse(''), 7000);
+    
+}
+const settingresponsenull =()=>{
+    setresponse('')
+}
+
   return (
-    <div className='w-full h-fit min-h-[70vh] flex justify-center items-center'>
+    <div className='w-full h-fit min-h-[70vh] flex relative  justify-center items-center'>
+    {
+        response &&    <div className='w-[250px] gap-5 animate-bounce text-xl  font-semibold  h-[100px] text-center bg-zinc-900 rounded-lg border-2 border-white  flex justify-center items-center    absolute top-0'> <div>{response}</div>
+        <button
+        onClick={settingresponsenull}
+         className='text-black bg-white px-2 py-1 font-bold'>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="currentColor"><path d="M10.5859 12L2.79297 4.20706L4.20718 2.79285L12.0001 10.5857L19.793 2.79285L21.2072 4.20706L13.4143 12L21.2072 19.7928L19.793 21.2071L12.0001 13.4142L4.20718 21.2071L2.79297 19.7928L10.5859 12Z"></path></svg>
+        </button>
+          </div> 
+    }
       <form onSubmit={handleSubmit} className="flex flex-col w-2/4 gap-5 justify-center items-center">
         <div className="formin w-full flex gap-3 flex-col">
           <fieldset className="border-2 border-white rounded-lg p-4">
@@ -76,6 +96,7 @@ const Contact = () => {
         </div>
         <input
           type="submit"
+          onClick={checkvalue}
           className="h-fit w-full   text-xl bg-white rounded-full border-none px-8 text-black"
           value="Contact"
         />
